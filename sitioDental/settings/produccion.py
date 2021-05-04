b70c5aff76e0c4e2010f2244766b53d68d169b36
unknown <drosalesm204@gmail.com>
2021-05-03 23:02:47 -0600

    credenciales

--- sitioDental/settings/produccion.py
+++ sitioDental/settings/produccion.py
@@ -9,10 +9,22 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog_dz',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
+        'NAME': 'dbi2c9j8ll5nm2',
+        'USER': 'rqccimwdngxfzh',
+        'PASSWORD': '3702d459e3788da74ad9f80b8e53c17f39f8eac428c2008e2c3366e14a91eac5',
+        'HOST': 'ec2-184-73-198-174.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
+
+
+DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'blog_dz',
+#        'USER': 'postgres',
+#        'PASSWORD': '123',
+#       'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
